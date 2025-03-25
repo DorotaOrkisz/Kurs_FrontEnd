4fbcf86e41db2a78041abdd00a993f17c29d5ef6
DorotaOrkisz <d_orotka@wp.pl>
2025-03-25 17:54:06 +0100

    js html fixed

--- karuzela/script.js
+++ karuzela/script.js
@@ -1,1 +1,21 @@
-console.log("Hello World");
+let active_item = 0;
+
+const right_arrow = document.querySelector(".right");
+const carousel = document.querySelector(".carousel");
+const carousel_items = carousel.children;
+
+function setItem(){
+    for(const item of carousel_items){
+        carousel_item.style.opacity = 0;
+    }
+    carousel_items[active_item].style.opacity = 1;
+}
+
+
+right_arrow.addEventListener("click", function(){
+    active_item++;
+    if(carousel_items.length = active_item){
+        active_item = 0;
+    }
+    setItem();
+});
